f5aa18712697a8f7be8a8e0ccd71391f08257e84
fengjw1 <fengjwxx@163.com>
2020-03-22 19:26:28 +0800

    init project

---
+++ app/src/main/java/com/example/navigationdemoofjava/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.navigationdemoofjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d("fengjw", "onFragmentInteraction>>uri>>" + uri.toString());
+    }
+}
